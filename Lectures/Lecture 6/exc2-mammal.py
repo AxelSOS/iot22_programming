50428fc5deef1e73f8a30ebfd26a329589276b12
Andreas Nilsson Ström <andreas.nilsson@natrolit.se>
2022-09-28 14:31:33 +0200

    Add mammal exercise to Lecture 6

---
+++ Lectures/Lecture 6/exc2-mammal.py
@@ -0,0 +1,19 @@
+# Övning 1
+#
+# Gör en klass "Mammal" som "Dog" ärver från
+# - Mammal har en ålder
+#
+# Gör en klass "Cat" som också ärver från "Mammal"
+# - Cat har 9 liv
+# - Cat har name, breed, sound
+# - Cat har metoder:
+#   ask_for_food()
+#      "Katten (name) ber om mat. (sound) (sound) (sound)"
+#   risky_jump()
+#      Slår en tärning (random)
+#      1 chans på 20 att katten förlorar ett av sina liv
+#
+#
+# Både katter och hundar ska ha en metod:
+# print_info()
+#  - Skriver ut alla attribut för djuret (inklusive ålder från superklassen)
